e5bf11e332bff0bcb68185292e8fb9fbb4543844
Daniel Vargas <50393869+dv1runner@users.noreply.github.com>
2019-11-02 14:35:38 -0700

    Add files via upload

---
+++ Destroyer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    //Destroys C4Rocks
+    // Start is called before the first frame update
+
+    [SerializeField]
+    GameObject prefabExplosion;
+
+    //timer support
+    Timer explodeTimer;
+
+    void Start()
+    {
+        explodeTimer = gameObject.AddComponent<Timer>();
+        explodeTimer.Duration = 1;
+        explodeTimer.Run();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for timer finished and restart
+        if (explodeTimer.Finished)
+        {
+            explodeTimer.Run();
+        }
+
+        //Blow up C4 Rock
+        GameObject rock = GameObject.FindWithTag("C4Rock");
+        if (rock != null)
+        {
+            Instantiate<GameObject>(prefabExplosion,
+                rock.transform.position, Quaternion.identity);
+            Destroy(rock);
+        }
+
+    }
+}
---
+++ Rock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // death support
+    const float RockLifespanSeconds = 10;
+    Timer deathTimer;
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+    void Start()
+    {
+        // apply impulse force to get teddy bear moving
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 5f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+
+        // create and start timer
+        deathTimer = gameObject.AddComponent<Timer>();
+        deathTimer.Duration = RockLifespanSeconds;
+        deathTimer.Run();
+    }
+
+    //fire when the rock has left the scene
+    void OnBecameInvisible()
+
+    {
+
+        //destroy the rock
+
+        Destroy(gameObject);
+
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+
+    }
+}
